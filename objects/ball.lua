82444e009b897f43ff9fc449201721b3672dc937
Aether <aetherallen1@gmail.com>
2024-09-14 22:17:37 +0100

    UPDATE ball.lua

--- objects/ball.lua
+++ objects/ball.lua
@@ -1,9 +1,20 @@
-function love.load()
+Ball = {
+    x = 0,
+    y = 0,
+    radius,
+    colour
+}
 
+function Ball:new(x, y, radius, colour)
+    ballTable = {
+        x=x,
+        y=y,
+        radius=radius,
+        colour=colour
+    }
+    setmetatable(ballTable, self)
+    self.__index = self
+    return ballTable
 end
 
-function love.update()
-end
-
-function love.draw()
-end
+return Ball
