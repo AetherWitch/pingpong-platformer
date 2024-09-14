9c2615ef88b524bacb2c82f9cadea8d22884feaa
Aether <aetherallen1@gmail.com>
2024-09-14 22:17:49 +0100

    UPDATE paddle.lua

--- objects/paddle.lua
+++ objects/paddle.lua
@@ -1,9 +1,24 @@
-function love.load()
+Paddle = {
+    x = 0,
+    y = 0,
+    width,
+    height,
+    colour,
+    speed
+}
 
+function Paddle:new(x, y, width, height, colour)
+    paddleTable = {
+        x=x,
+        y=y,
+        width=width,
+        height=height,
+        colour=colour,
+        speed=speed
+    }
+    setmetatable(paddleTable, self)
+    self.__index = self
+    return paddleTable
 end
 
-function love.update()
-end
-
-function love.draw()
-end
+return Paddle
